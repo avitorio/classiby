9ec2dcf33ffb2abe5ac93c26eafc9b611a012e82
Andre Vitorio <andre@vitorio.net>
2020-01-14 23:15:37 -0300

    Created User Schema

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,23 @@
 const { Router } = require('express');
+const User = require('./models/User');
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ text: 'Heao' });
+routes.post('/users', async (req, res) => {
+  const { name, email, latitude, longitude } = req.body;
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+  const user = await User.create({
+    name,
+    email,
+    location,
+  });
+
+  return res.json(user);
 });
 
 module.exports = routes;
