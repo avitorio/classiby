924fd0945269b3d807b78a349c60dad53a613b90
Andre Vitorio <andre@vitorio.net>
2020-01-14 22:41:30 -0300

    Added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-o5iks.mongodb.net/classiby?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   return response.json({ text: "Heao" });
